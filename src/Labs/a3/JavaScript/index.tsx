b617f362ec3ef5d3de4e41e4fe438aefa0893fbf
dudleyce <dudley.c@northeastern.edu>
2024-02-14 09:36:29 -0500

    Start of a3

---
+++ src/Labs/a3/JavaScript/index.tsx
@@ -0,0 +1,33 @@
+import MapFunction from "./arrays/MapFunction";
+import WorkingWithArrays from "./arrays/WorkingWithArrays";
+import IfElse from "./conditionals/IfElse";
+import TernaryOperator from "./conditionals/TernaryOperator";
+import ArrowFunctions from "./functions/ArrowFunctions";
+import ES5Functions from "./functions/ES5functions";
+import FunctionParenthesisAndParemeters from "./functions/FunctionParenthesisAndParameters";
+import ImpliedReturn from "./functions/ImpliedReturn";
+import BooleanVariables from "./variables/BooleanVariables";
+import VariableTypes from "./variables/VariableTypes";
+import VariablesAndConstants from "./variables/VariablesAndConstants";
+
+function JavaScript() {
+    console.log('Hello World!');
+    return (
+        <div>
+            <h3>JavaScript</h3>
+            <MapFunction />
+            <WorkingWithArrays />
+            <FunctionParenthesisAndParemeters />
+            <ImpliedReturn />
+            <ArrowFunctions />
+            <ES5Functions />
+            <TernaryOperator />
+            <IfElse />
+            <BooleanVariables />
+            <VariableTypes />
+            <VariablesAndConstants />
+        </div>
+    )
+}
+
+export default JavaScript;
